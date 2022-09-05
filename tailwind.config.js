41e9c439f62d6f40c5146a5b1ea43ff350dded09
davinzaki <davinzaki12@gmail.com>
2022-09-05 12:46:07 +0700

    world vision

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+    variants: {
+      extend:{
+        display: ['group-focus'],
+        opacity: ['group-focus'],
+        inset:  ['group-focus']
+      },
+    },
+  plugins: [],
+}
